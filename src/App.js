28e32ca8eb3effa0b962c424a7d161389114f90d
MAYANK CHETAN PARVATIA <104015658+mayank2368@users.noreply.github.com>
2023-03-08 12:54:54 +0100

    build the small demo-test app using the react with node server and mongoDB database. Just adding, fetching and saving the data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [form, setForm] = useState({});
+  const [users, setUsers] = useState([]);
+  const handleChange = (e) => {
+    //console.log(e.target.value, e.target.name);
+
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:8080/demo", {
+      method: "POST",
+      body: JSON.stringify(form),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+  };
+
+  const getUsers = async () => {
+    const response = await fetch("http://localhost:8080/demo", {
+      method: "GET",
+    });
+    const data = await response.json();
+    setUsers(data);
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+  //  <p>{JSON.stringify(form)}</p>
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="full-stack">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username: </label>
+        <input
+          type="text"
+          name="username"
+          id="username"
+          onChange={handleChange}
+        ></input>
+        <br />
+        <label htmlFor="password">Password: </label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          onChange={handleChange}
+        ></input>
+        <br />
+        <input type={"submit"}></input>
+        <br />
+      </form>
+      <div>
+        <ul>
+          {users.map((user) => (
+            <li key={user._id}>{user.username}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
